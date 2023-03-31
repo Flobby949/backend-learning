231e3487514d1c412ccb7959fa82759d2ad87b8c
jinchenxing <2541226493@qq.com>
2023-03-31 10:03:20 +0800

    Spring Security 学习项目初始化

---
+++ spring-security-learning/src/test/java/top/flobby/security/SpringSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package top.flobby.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
