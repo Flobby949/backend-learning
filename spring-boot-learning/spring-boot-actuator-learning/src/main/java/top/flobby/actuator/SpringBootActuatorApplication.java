9dad237f0c732a151d4ffcc7217fe70739bfbfd3
jinchenxing <2541226493@qq.com>
2023-04-18 15:18:31 +0800

    整合 Actuator

---
+++ spring-boot-learning/spring-boot-actuator-learning/src/main/java/top/flobby/actuator/SpringBootActuatorApplication.java
@@ -0,0 +1,18 @@
+package top.flobby.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Flobby
+ * @program : spring-boot-learning
+ * @description :
+ * @create : 2023-04-18 14:57
+ **/
+
+@SpringBootApplication
+public class SpringBootActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuatorApplication.class, args);
+    }
+}
---
+++ spring-boot-learning/spring-boot-actuator-learning/src/main/java/top/flobby/actuator/service/TestService.java
@@ -0,0 +1,14 @@
+package top.flobby.actuator.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : Flobby
+ * @program : spring-boot-learning
+ * @description :
+ * @create : 2023-04-18 15:07
+ **/
+
+@Service
+public class TestService {
+}
