afceabc060dc7582edcdeb82648efc025c29cf43
jinchenxing <2541226493@qq.com>
2023-04-23 17:18:27 +0800

    feat(custom): 基础核心模块 common 通用类

---
+++ admin-api-template/api-base/src/main/java/top/flobby/common/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package top.flobby.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author : Flobby
+ * @program : admin-api-template
+ * @description : 错误编码
+ * @create : 2023-04-23 17:00
+ **/
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    UNAUTHORIZED(401, "未授权，不能访问"),
+    FORBIDDEN(403, "没有权限，禁止访问"),
+    INTERNAL_SERVER_ERROR(500, "服务器异常，请稍后再试");
+
+    private final int code;
+    private final String msg;
+}
