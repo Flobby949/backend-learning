ffccc243b99233e494f908476e3cb1794206a317
jinchenxing <2541226493@qq.com>
2023-04-23 17:25:42 +0800

    feat(custom): 基础核心模块 security 相关配置类

---
+++ admin-api-template/api-base/src/main/java/top/flobby/security/cache/TokenStoreCache.java
@@ -0,0 +1,35 @@
+package top.flobby.security.cache;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import top.flobby.common.cache.RedisCache;
+import top.flobby.security.user.UserDetail;
+import top.flobby.common.cache.RedisKeys;
+
+ /**
+ * @author : Flobby
+ * @program : admin-api-template
+ * @description : 登录用户的缓存操作
+ * @create : 2023-04-23 17:16
+ **/
+@Component
+@AllArgsConstructor
+public class TokenStoreCache {
+
+    private final RedisCache redisCache;
+
+    public void saveUser(String accessToken, UserDetail user) {
+        String key = RedisKeys.getAccessTokenKey(accessToken);
+        redisCache.set(key, user);
+    }
+
+    public UserDetail getUser(String accessToken) {
+        String key = RedisKeys.getAccessTokenKey(accessToken);
+        return (UserDetail) redisCache.get(key);
+    }
+
+    public void deleteUser(String accessToken) {
+        String key = RedisKeys.getAccessTokenKey(accessToken);
+        redisCache.delete(key);
+    }
+}
---
+++ admin-api-template/api-base/src/main/java/top/flobby/security/config/SecurityConfig.java
@@ -0,0 +1,95 @@
+package top.flobby.security.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+import top.flobby.security.exception.SecurityAuthenticationEntryPoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : Flobby
+ * @program : admin-api-template
+ * @description : Spring Security 配置
+ * @create : 2023-04-23 17:16
+ **/
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final OncePerRequestFilter authenticationTokenFilter;
+    private final PermitResource permitResource;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+
+        return daoAuthenticationProvider;
+    }
+
+
+    /**
+     * 身份验证管理器
+     * 实现账号密码登录
+     *
+     * @return {@link AuthenticationManager}
+     */
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        List<AuthenticationProvider> providerList = new ArrayList<>();
+        providerList.add(daoAuthenticationProvider());
+
+        ProviderManager providerManager = new ProviderManager(providerList);
+        providerManager.setAuthenticationEventPublisher(new DefaultAuthenticationEventPublisher(applicationEventPublisher));
+
+        return providerManager;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // 忽略授权的地址列表
+        List<String> permitList = permitResource.getPermitList();
+        String[] permits = permitList.toArray(new String[0]);
+
+        http
+                // 把 token 过滤器配置在 usernamePassword 过滤器的前面，就支持 token 认证了
+                .addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                // STATELESS 无状态：前后端分离开发配置
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests(auth -> auth
+                        .requestMatchers(permits).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling().authenticationEntryPoint(new SecurityAuthenticationEntryPoint())
+                .and().headers().frameOptions().disable()
+                .and().csrf(AbstractHttpConfigurer::disable)
+        ;
+
+        return http.build();
+    }
+}
