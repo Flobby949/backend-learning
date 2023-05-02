34e959ca364d07139a1092e2f1680a780165d3c4
jinchenxing <2541226493@qq.com>
2023-05-03 01:03:55 +0800

    feat(custom): 文件上传接口

---
+++ admin-api-template/api-admin-core/src/main/java/top/flobby/rbac/controller/SysFileUploadController.java
@@ -0,0 +1,46 @@
+package top.flobby.rbac.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import top.flobby.common.utils.Result;
+import top.flobby.rbac.vo.SysFileUploadVO;
+import top.flobby.storage.service.StorageService;
+
+/**
+ * @author : Flobby
+ * @program : admin-api-template
+ * @description : 文件上传接口
+ * @create : 2023-05-03 01:00
+ **/
+
+@RestController
+@RequestMapping("sys/file")
+@Tag(name = "文件上传")
+@AllArgsConstructor
+public class SysFileUploadController {
+    private final StorageService storageService;
+
+    @PostMapping("upload")
+    @Operation(summary = "上传")
+    public Result<SysFileUploadVO> upload(@RequestParam("file") MultipartFile file) throws Exception {
+        if (file.isEmpty()) {
+            return Result.error("请选择需要上传的文件");
+        }
+        // 上传路径
+        String path = storageService.getPath(file.getOriginalFilename());
+        // 上传文件
+        String url = storageService.upload(file.getBytes(), path);
+        SysFileUploadVO vo = new SysFileUploadVO();
+        vo.setUrl(url);
+        vo.setSize(file.getSize());
+        vo.setName(file.getOriginalFilename());
+        vo.setPlatform(storageService.properties.getConfig().getType().name());
+        return Result.ok(vo);
+    }
+}
--- admin-api-template/api-server/src/test/java/top/flobby/FileUploadTest.java
+++ admin-api-template/api-server/src/test/java/top/flobby/FileUploadTest.java
@@ -1,28 +1,20 @@
 package top.flobby;
 
-import cn.hutool.core.io.IoUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import top.flobby.storage.service.StorageService;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 
 @SpringBootTest
 @Slf4j
 public class FileUploadTest {
-    @Autowired
-    private StorageService storageService;
-
-    @Test
-    void uploadTest() throws IOException {
-        File file = new File("C:\\Users\\Flobby\\Desktop\\MY\\avatar\\1.jpg");
-        byte[] bytes = IoUtil.readBytes(Files.newInputStream(file.toPath()));
-        String path = storageService.getPath(file.getName());
-        String url = storageService.upload(bytes, path);
-        log.info(url);
-    }
+    // @Autowired
+    // private StorageService storageService;
+    //
+    // @Test
+    // void uploadTest() throws IOException {
+    //     File file = new File("C:\\Users\\Flobby\\Desktop\\MY\\avatar\\1.jpg");
+    //     byte[] bytes = IoUtil.readBytes(Files.newInputStream(file.toPath()));
+    //     String path = storageService.getPath(file.getName());
+    //     String url = storageService.upload(bytes, path);
+    //     log.info(url);
+    // }
 }
